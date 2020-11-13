d332741e24f415d18cc27a5940d1cca5235595df
hartmluk <lukas.hartmann@student.uni-augsburg.de>
2020-11-13 17:54:09 +0100

    add full program

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,46 @@
-public class Program {
+import java.util.ArrayList;
+
+class Program {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        boolean weitereAnlegen = true;
+        ArrayList<Person> personen = new ArrayList<Person>();
+
+        while (weitereAnlegen) {
+
+            Person person = new Person();
+
+            System.out.println("Vorname eingeben:");
+            String vorname = System.console().readLine();
+
+            person.setVorname(vorname);
+            personen.add(person);
+
+            System.out.println("Weitere anlegen (y/n)?");
+            String weitere = System.console().readLine();
+            if (weitere.equals("n"))
+                weitereAnlegen = false;
+        }
+
+        System.out.println("Angelete Personen: ");
+        for (Person p : personen)
+            System.out.println("Vorname der Person: " + p.getVorname());
+    }
+}
+
+class Person {
+    String vorname;
+    String nachname;
+
+    Person() {
+
+    }
+
+    void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    String getVorname() {
+        return vorname;
     }
 }
